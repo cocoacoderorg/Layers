5a7ac8ad91c555967e1b431609c22722e1eef732
Jim Hillhouse <jimhillhouse@me.com>
2011-03-06 14:56:07 -0600

    Initial Commit

---
+++ Layers/LayersAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  LayersAppDelegate.h
+//  Layers
+//
+//  Created by James Hillhouse on 3/6/11.
+//  Copyright 2011 PortableFrontier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LayersAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ Layers/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Layers
+//
+//  Created by James Hillhouse on 3/6/11.
+//  Copyright 2011 PortableFrontier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
