aa616ac52ee3202071f2a85f7a9ac5eacc42c62d
Jim Hillhouse <jdhouse4@Jims-Mac-mini.local>
2011-08-26 18:52:07 -0500

    Just a commit

--- Layers/LayersRootViewController.h
+++ Layers/LayersRootViewController.h
@@ -8,9 +8,43 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RootViewController : UITableViewController {
 
+@class LayerFunViewController1;
+@class LayerFunViewController2;
+@class LayerFunViewController3;
+@class LayerFunViewController4;
+@class LayerFunViewController5;
+@class LayerFunViewController6;
+
+
+@interface LayersRootViewController : UITableViewController <UINavigationBarDelegate, UITableViewDelegate, UITableViewDataSource>
+{
+    NSArray                         *viewControllers;
+    
+    LayerFunViewController1         *viewController1;
+    LayerFunViewController2         *viewController2;
+    LayerFunViewController3         *viewController3;
+    LayerFunViewController4         *viewController4;
+    LayerFunViewController5         *viewController5;
+    LayerFunViewController6         *viewController6;
+
+    UITableViewCell                 *layerLessonsTableViewCell;
+    UILabel                         *layerLessonName;
+    UILabel                         *layerLessonSummary;
 }
+
+@property (nonatomic, retain)                       NSArray                     *viewControllers;
+
+@property (nonatomic, retain)       IBOutlet        LayerFunViewController1     *viewController1;
+@property (nonatomic, retain)       IBOutlet        LayerFunViewController2     *viewController2;
+@property (nonatomic, retain)       IBOutlet        LayerFunViewController3     *viewController3;
+@property (nonatomic, retain)       IBOutlet        LayerFunViewController4     *viewController4;
+@property (nonatomic, retain)       IBOutlet        LayerFunViewController5     *viewController5;
+@property (nonatomic, retain)       IBOutlet        LayerFunViewController6     *viewController6;
+
+@property (nonatomic, retain)       IBOutlet        UITableViewCell             *layerLessonsTableViewCell;
+@property (nonatomic, retain)       IBOutlet        UILabel                     *layerLessonName;
+@property (nonatomic, retain)       IBOutlet        UILabel                     *layerLessonSummary;
 
 
 @end
--- Layers/LayersRootViewController.m
+++ Layers/LayersRootViewController.m
@@ -6,42 +6,144 @@
 //  Copyright 2011 PortableFrontier. All rights reserved.
 //
 
-#import "RootViewController.h"
+#import "LayersRootViewController.h"
 
-@implementation RootViewController
+
+
+@implementation LayersRootViewController
+
+
+@synthesize viewControllers;
+
+@synthesize viewController1;
+@synthesize viewController2;
+@synthesize viewController3;
+@synthesize viewController4;
+@synthesize viewController5;
+@synthesize viewController6;
+
+@synthesize layerLessonsTableViewCell;
+@synthesize layerLessonName;
+@synthesize layerLessonSummary;
+
+
+- (void)dealloc
+{
+    [viewControllers release];
+    
+    [viewController1 release];
+    [viewController2 release];
+    [viewController3 release];
+    [viewController4 release];
+    [viewController5 release];
+    [viewController6 release];
+    
+    [layerLessonsTableViewCell release];
+    [layerLessonName release];
+    [layerLessonSummary release];
+    
+    [super dealloc];
+}
+
+
+
+
+#pragma mark -
+#pragma mark UIViewController Methods
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    NSDictionary    *layerDictionary1   = [[NSDictionary alloc] initWithObjectsAndKeys:@"Example 1", @"Name",
+                                           @"An image without perspective. How boring.", @"Summary", nil];
+
+    NSDictionary    *layerDictionary2   = [[NSDictionary alloc] initWithObjectsAndKeys:@"Example 2", @"Name",
+                                           @"An image with correct proportions. Slightly less boring.", @"Summary", nil];
+    
+    NSDictionary    *layerDictionary3   = [[NSDictionary alloc] initWithObjectsAndKeys:@"Example 3", @"Name",
+                                           @"Now our image has perspective. We're making progress", @"Summary", nil];
+    
+    NSDictionary    *layerDictionary4   = [[NSDictionary alloc] initWithObjectsAndKeys:@"Example 4", @"Name",
+                                           @"Adjustable perspective and layer distance. Not too bad.", @"Summary", nil];
+    
+    NSDictionary    *layerDictionary5   = [[NSDictionary alloc] initWithObjectsAndKeys:@"Example 5", @"Name",
+                                           @"Adjustable tilt, perspective and layer distance. The more, the merrier.", @"Summary", nil];
+    
+    NSDictionary    *layerDictionary6   = [[NSDictionary alloc] initWithObjectsAndKeys:@"Example 6", @"Name",
+                                               @"Using gestures to manipulate angle/rotation, layer distance & perspective.", @"Summary", nil];
+    
+    NSArray         *anArray            = [[NSArray alloc] initWithObjects:
+                                           layerDictionary1, 
+                                           layerDictionary2, 
+                                           layerDictionary3, 
+                                           layerDictionary4, 
+                                           layerDictionary5, 
+                                           layerDictionary6, nil];
+    self.viewControllers                = anArray;
+    
+    [layerDictionary1 release];
+    [layerDictionary2 release];
+    [layerDictionary3 release];
+    [layerDictionary4 release];
+    [layerDictionary5 release];
+    [layerDictionary6 release];
+    [anArray release];
 }
+
+
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
 }
 
+
+
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
 }
+
+
 
 - (void)viewWillDisappear:(BOOL)animated
 {
 	[super viewWillDisappear:animated];
 }
 
+
+
 - (void)viewDidDisappear:(BOOL)animated
 {
 	[super viewDidDisappear:animated];
 }
 
-/*
- // Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-	// Return YES for supported orientations.
-	return (interfaceOrientation == UIInterfaceOrientationPortrait);
+
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    
+    self.viewControllers            = nil;
+    
+    self.viewController1            = nil;
+    self.viewController2            = nil;
+    self.viewController3            = nil;
+    self.viewController4            = nil;
+    self.viewController5            = nil;
+    self.viewController6            = nil;
+    
+    self.layerLessonsTableViewCell  = nil;
+    self.layerLessonName            = nil;
+    self.layerLessonSummary         = nil;
 }
- */
+
+
+
+
+#pragma mark
+#pragma mark UITableViewController Methods
 
 // Customize the number of sections in the table view.
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -49,76 +151,95 @@
     return 1;
 }
 
+
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 0;
+    return [self.viewControllers count];
 }
+
+
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *LayerLessonsCellIdentifier = @"Cell";
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+    UITableViewCell *cell           = [tableView dequeueReusableCellWithIdentifier:LayerLessonsCellIdentifier];
+    if (cell == nil) 
+    {
+        NSArray    *nibArray        = [[NSBundle mainBundle] loadNibNamed:@"LayerLessonsTableViewCell" owner:self options:nil];
+        
+        if ( [nibArray count] > 0 ) 
+        {
+            cell    = self.layerLessonsTableViewCell;
+        }
+        else
+        {
+            NSLog(@"Uh oh, LayerLessonsTableViewCell nib file didn't load.");
+        }
     }
 
     // Configure the cell.
+    NSUInteger row                  = [indexPath row];
+    NSDictionary *rowData           = [self.viewControllers objectAtIndex:row];
+    
+    self.layerLessonName.text       = [rowData objectForKey:@"Name"];
+    self.layerLessonSummary.text    = [rowData objectForKey:@"Summary"];
+    
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete)
-    {
-        // Delete the row from the data source.
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }
-    else if (editingStyle == UITableViewCellEditingStyleInsert)
-    {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }   
-}
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+	return 100;
 }
-*/
 
-/*
+
+
 // Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
+//- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    // Return NO if you do not want the item to be re-orderable.
+//    return YES;
+//}
+
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    /*
-    <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-    // ...
-    // Pass the selected object to the new view controller.
-    [self.navigationController pushViewController:detailViewController animated:YES];
-    [detailViewController release];
-	*/
+    NSUInteger row                  = [indexPath row];
+
+    if ( row == 0 ) 
+    {
+         [self.navigationController pushViewController:self.viewController1 animated:YES];
+    }
+    if ( row == 1 ) 
+    {
+        [self.navigationController pushViewController:self.viewController2 animated:YES];
+    }    
+    if ( row == 2 ) 
+    {
+        [self.navigationController pushViewController:self.viewController3 animated:YES];
+    }    
+    if ( row == 3 ) 
+    {
+        [self.navigationController pushViewController:self.viewController4 animated:YES];
+    }
+    if ( row == 4 ) 
+    {
+        [self.navigationController pushViewController:self.viewController5 animated:YES];
+    }
+    if ( row == 5 ) 
+    {
+        [self.navigationController pushViewController:self.viewController6 animated:YES];
+    }
+
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
+
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -126,19 +247,6 @@
     [super didReceiveMemoryWarning];
     
     // Relinquish ownership any cached data, images, etc that aren't in use.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-
-    // Relinquish ownership of anything that can be recreated in viewDidLoad or on demand.
-    // For example: self.myOutlet = nil;
-}
-
-- (void)dealloc
-{
-    [super dealloc];
 }
 
 @end
